0a1cae0e696974d3326d26fef1dd2375502a6f40
rezaarezvan <reza@rezvan.xyz>
2024-02-23 16:36:45 +0100

    random mnist

--- examples/logistic_random_mnist.py
+++ examples/logistic_random_mnist.py
@@ -1,78 +1,105 @@
 import torch
+import torch.nn as nn
+import torchvision
 import torchvision.transforms as transforms
-from torchvision import datasets
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-
-torch.manual_seed(0)
-
-train_dataset = datasets.MNIST(
-    root='../extra/datasets', train=True, transform=transforms.ToTensor(), download=True)
-test_dataset = datasets.MNIST(
-    root='../extra/datasets', train=False, transform=transforms.ToTensor())
-
-print("Number of training samples: " + str(len(train_dataset)) + "\n" +
-      "Number of test samples: " + str(len(test_dataset)))
-
-batch_size = 32
-train_loader = DataLoader(dataset=train_dataset,
-                          batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset,
-                         batch_size=batch_size, shuffle=False)
+import torch.optim as optim
 
 
+def load_data():
+    trainset = torchvision.datasets.MNIST(
+        root='../extra/datasets', train=True, download=True, transform=transforms.ToTensor())
+    testset = torchvision.datasets.MNIST(
+        root='../extra/datasets', train=False, download=True, transform=transforms.ToTensor())
 
-class LogisticRegression(torch.nn.Module):
-    def __init__(self, n_inputs, n_outputs, sigma=0.01):
-        super().__init__()
-        self.linear = torch.nn.Linear(n_inputs, n_outputs)
-        self.sigma = sigma
+    trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+    testloader = DataLoader(testset, batch_size=64, shuffle=False)
+
+    return trainloader, testloader
+
+
+class LogisticRegression(nn.Module):
+    def __init__(self, n_inputs, n_outputs):
+        super(LogisticRegression, self).__init__()
+        self.linear = nn.Linear(n_inputs, n_outputs, bias=False)
 
     def forward(self, x):
-        noise = torch.randn(28**2) + self.sigma
-        x    += x + noise
         return self.linear(x)
 
 
-n_inputs = 28 * 28
-n_outputs = 10
-log_regr = LogisticRegression(n_inputs, n_outputs)
-optimizer = torch.optim.Adam(log_regr.parameters(), lr=0.0005)
-criterion = torch.nn.CrossEntropyLoss()
+def train_model(model, train, test, optimizer, epochs=3):
+    loss = nn.CrossEntropyLoss()
+    acc = 0
 
-epochs = 10
-Loss = []
-acc = []
-for epoch in range(epochs):
-    for i, (images, labels) in enumerate(train_loader):
-        optimizer.zero_grad()
-        outputs = log_regr(images.view(-1, 28*28))
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        Loss.append(loss.item())
+    for epoch in range(epochs):
+        model.train()
+        for i, (data, target) in enumerate(train):
+            optimizer.zero_grad()
+            input = model(data.view(-1, 28*28))
+            output = loss(input, target)
+            output.backward()
+            optimizer.step()
+            if i % 100 == 0:
+                print(f'Epoch {epoch+1}, Batch {i}, Loss: {output.item()}')
 
-    log_regr.eval()
+        print(f'Epoch {epoch+1} completed')
+        print('Testing model...')
+        acc = test_model(model, test)
+        print('')
+
+    return acc
+
+
+def test_model(model, test):
+    loss = nn.CrossEntropyLoss()
+
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
     with torch.no_grad():
-        correct = 0
-        for images, labels in test_loader:
-            outputs = log_regr(images.view(-1, 28*28))
-            _, predicted = torch.max(outputs.data, 1)
-            correct += (predicted == labels).sum()
-        accuracy = 100 * (correct.item()) / len(test_dataset)
-        acc.append(accuracy)
-    print('Epoch: {}. Last Batch Loss: {}. Accuracy: {}'.format(
-        epoch, Loss[-1], accuracy))
-    log_regr.train()
+        for data, target in test:
+            input = model(data.view(-1, 28*28))
+            test_loss += loss(input, target).item()
+            pred = input.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-plt.plot(Loss)
-plt.xlabel("Number of Batches")
-plt.ylabel("Loss")
-plt.title("Training Loss per Batch")
-plt.show()
+    test_loss /= len(test.dataset)
+    acc = 100. * correct / len(test.dataset)
+    print(f'Average loss: {test_loss:.4f}, Accuracy: {
+          correct}/{len(test.dataset)} ({acc:.2f}%)')
 
-plt.plot(acc)
-plt.xlabel("Epochs")
-plt.ylabel("Accuracy (%)")
-plt.title("Test Accuracy per Epoch")
-plt.show()
+    return acc
+
+
+def main():
+    n_inputs = 28 * 28
+    n_outputs = 10
+    train, test = load_data()
+    model = LogisticRegression(n_inputs, n_outputs)
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    no_noise_acc = train_model(model, train, test, optimizer, epochs=5)
+
+    for param in model.parameters():
+        print(param)
+        print(param.shape)
+
+    sigma = 0.5
+    noise_weights = torch.rand(10, 28*28) * sigma
+    for param in model.parameters():
+        param.data += noise_weights
+
+    for param in model.parameters():
+        print(param)
+        print(param.shape)
+
+    noise_acc = test_model(model, test)
+
+    print(f'Non-troll accuracy: {no_noise_acc}')
+    print(f'Troll accuracy: {noise_acc}')
+    print(f'Accuracy difference: {no_noise_acc - noise_acc}')
+
+
+if __name__ == '__main__':
+    main()
