09f4ac2b44067be36dbf89c3f731cbd3d8d58a72
Nils Ekström <nilsek@chalmers.se>
2024-03-07 10:59:30 +0100

    Fixade weighted avg. plots m.m.
    Flyttade även ett par funktioner till en utils map för jag höll på få en stroke :)

--- examples/entropy/entropy_mnist.py
+++ examples/entropy/entropy_mnist.py
@@ -8,6 +8,7 @@ from torch.distributions import Categorical
 import torch.optim as optim
 import numpy as np
 import sys
+from utils.utils import load_model, save_model, test_model_noise, calculate_weighted_averages, plot_weighted_averages, plot_entropy_prob
 
 model_path = 'model_state/entropy_mnist.pth'
 
@@ -30,7 +31,6 @@ def load_data():
     test = DataLoader(test, batch_size=32, shuffle=False)
 
     return train, test
-
 
 class LogisticRegression(nn.Module):
     def __init__(self, n_inputs, n_outputs):
@@ -82,134 +82,6 @@ def test_model(model, test):
     print(f'Accuracy: {correct}/{len(test.dataset)} ({acc: .2f} %)\n')
     return acc
 
-
-def save_model_parameters(model):
-    original_params = [param.clone() for param in model.parameters()]
-    return original_params
-
-
-@torch.no_grad()
-def restore_model_parameters(model, original_params):
-    for param, original in zip(model.parameters(), original_params):
-        param.copy_(original)
-
-
-@torch.no_grad()
-def add_noise(model, sigma):
-    for param in model.parameters():
-        param += torch.randn_like(param) * sigma
-    return model
-
-
-def calculate_entropy(predictions: torch.Tensor):
-    """
-    Follows this entropy function:
-    https://en.wikipedia.org/wiki/Entropy_(information_theory)
-    log_e
-    The function works by first calculating the quantity of the number
-    in the list and creates a list where it's frequency is placed in that list.
-    For example:
-    [1,7,1,7,1]->[0,3,0,0,0,0,0,2]/len([1,7,1,7,1])->[0,(3/5),0,0,0,0,0,(2/5)]
-    -------------------------------------------------------------------------
-    Perform entropy calculation on every row in the predictions matrix
-    """
-    entropy = torch.zeros(predictions.size(0))
-    for i in range(predictions.size(0)):
-        probability  = torch.bincount(predictions[i], minlength=10).float()
-        probability /= predictions.size(1) # Normalize
-        entropy[i]   = Categorical(probs=probability).entropy()
-    return entropy
-
-@torch.no_grad()
-def test_model_noise(model, dataset, sigma=0, iters=10):
-    model.eval()
-    result = []
-
-    print(f"Testing model with noise scale {sigma:.2f}...")
-    for batch, (data, target) in enumerate(dataset):
-        predictions = []
-        data, target = data.to(DEVICE), target.to(DEVICE)
-        for _ in range(iters):
-            original_params = save_model_parameters(model)
-            add_noise(model, sigma)
-
-            output = model(data.view(-1, 28 * 28))
-            pred   = torch.softmax(output, dim=1).argmax(dim=1)
-
-            predictions.append(pred)
-            restore_model_parameters(model, original_params)
-        
-        pred_matrix = torch.stack(predictions, dim=1)
-        entropy     = calculate_entropy(pred_matrix)
-        accuracy    = (pred_matrix == target.view(-1, 1)).float().mean(dim=1)
-        
-        for e, a in zip(entropy, accuracy):
-            result.append((e.item(), a.item()))
-
-        if (batch + 1) % 50 == 0:
-            print(f'Batch {batch + 1}/{len(dataset)} completed')
-
-    print(f'Batch {batch+1}/{len(dataset)} completed')
-    print(f'Testing model with noise scale {sigma:.2f} completed\n')
-    return result
-
-
-def plot_entropy_prob(ent_prob, sigma, acc, iterations):
-    """
-    Plots the list of probability/entropy parts on the x/y-axis respectively.
-    """
-    entropy, probability = zip(*ent_prob)
-
-    plt.figure(figsize=(10, 6))
-    plt.scatter(probability, entropy, alpha=0.1)
-    plt.xlabel('Probability')
-    plt.ylabel('Entropy')
-    plt.title(f'Entropy to Probability (sigma={sigma}, iterations={iterations}, {acc}%)')
-    plt.grid()
-    plt.ylim(-0.1, np.log(10) + 0.1)
-    plt.xlim(-0.1, 1.1)
-    plot_curve(classes=10)
-    if SAVE_PLOT: plt.savefig(f'plots/entropy_prob_sigma_{sigma:.2f}.pdf')
-    else: plt.show()
-
-
-def plot_curve(classes):
-    """
-    This call plots the domain of the amount of given classes.
-    (Beware there exists off-by-one, this is handled in the function)
-    """
-    classes -= 1
-    # Arc-function at bottom
-    x = np.linspace(0.0001, 0.9999, 10000)
-    y = -x * np.log(x) - (1 - x) * np.log(1 - x)
-    plt.plot(x, y, color='orange')
-    x = np.linspace(0.0001, 0.9999, 10000)
-    y = -x * np.log(x) - (1 - x) * np.log(1 - x)
-
-    # Higher top function
-    y2 = []
-    for p in x:
-        a = -p * np.log(p)
-        for i in range(classes):
-            a -= (1 - p) / (classes) * np.log((1 - p) / classes)
-        y2.append(a)
-
-    plt.plot(x, y, color="orange")
-    plt.plot(x, y2, color="orange")
-
-    # This plots the vertical line
-    plt.plot([0, 0], [0, y2[0]], color="orange")
-
-
-def save_model(model, path=model_path):
-    torch.save(model.state_dict(), path)
-    print(f'Model saved to {path}')
-
-def load_model(model, path=model_path):
-    model.load_state_dict(torch.load(path))
-    print(f'Model loaded from {path}')
-    return model
-
 def main():
     n_inputs = 28 * 28
     n_outputs = 10
@@ -222,25 +94,28 @@ def main():
     criterion = nn.CrossEntropyLoss()
 
     if LOAD_WEIGHTS:
-        model = load_model(model)
+        model = load_model(model, model_path)
     else:
         train_model(model, train, test, optimizer, criterion, epochs=1)
     
     if SAVE_WEIGHTS:
-        save_model(model)
+        save_model(model, model_path)
     
     accuracy = test_model(model, test)
     iterations = 100
     entropies = []
 
-    sigmas = np.arange(0, 1, 0.25)
+    weighted_average = []
+    sigmas = np.arange(0, 1, 0.1)
     sigmas = np.append(sigmas, [1, 10, 100, 500])
     for sigma in sigmas:
         entropy = test_model_noise(model, test, sigma=sigma, iters=iterations)
+        weighted_average.append((calculate_weighted_averages(entropy), sigma))
         entropies.append(entropy)
-    
+   
+    plot_weighted_averages(weighted_average)
     for entropy, sigma in zip(entropies, sigmas):
-        plot_entropy_prob(entropy, sigma, accuracy, iterations)
+        plot_entropy_prob(entropy, sigma, accuracy, iterations, SAVE_PLOT=SAVE_PLOT)
 
 
 if __name__ == '__main__':
---
+++ examples/entropy/utils/utils.py
@@ -0,0 +1,192 @@
+from torch.distributions import Categorical 
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def save_model_parameters(model):
+    original_params = [param.clone() for param in model.parameters()]
+    return original_params
+
+
+@torch.no_grad()
+def restore_model_parameters(model, original_params):
+    for param, original in zip(model.parameters(), original_params):
+        param.copy_(original)
+
+def save_model(model, path):
+    torch.save(model.state_dict(), path)
+    print(f'Model saved to {path}')
+
+def load_model(model, path):
+    model.load_state_dict(torch.load(path))
+    print(f'Model loaded from {path}')
+    return model
+
+@torch.no_grad()
+def add_noise(model, sigma):
+    for param in model.parameters():
+        param += torch.randn_like(param) * sigma
+    return model
+
+def calculate_entropy(predictions: torch.Tensor):
+    """
+    Follows this entropy function:
+    https://en.wikipedia.org/wiki/Entropy_(information_theory)
+    log_e
+    The function works by first calculating the quantity of the number
+    in the list and creates a list where it's frequency is placed in that list.
+    For example:
+    [1,7,1,7,1]->[0,3,0,0,0,0,0,2]/len([1,7,1,7,1])->[0,(3/5),0,0,0,0,0,(2/5)]
+    -------------------------------------------------------------------------
+    Perform entropy calculation on every row in the predictions matrix
+    """
+    entropy = torch.zeros(predictions.size(0))
+    for i in range(predictions.size(0)):
+        probability  = torch.bincount(predictions[i], minlength=10).float()
+        probability /= predictions.size(1) # Normalize
+        entropy[i]   = Categorical(probs=probability).entropy()
+    return entropy
+
+@torch.no_grad()
+def test_model_noise(model, dataset, sigma=0, iters=10):
+    model.eval()
+    result = []
+
+    print(f"Testing model with noise scale {sigma:.2f}...")
+    for batch, (data, target) in enumerate(dataset):
+        predictions = []
+        data, target = data.to(DEVICE), target.to(DEVICE)
+        for _ in range(iters):
+            original_params = save_model_parameters(model)
+            add_noise(model, sigma)
+
+            output = model(data.view(-1, 28 * 28))
+            pred   = torch.softmax(output, dim=1).argmax(dim=1)
+
+            predictions.append(pred)
+            restore_model_parameters(model, original_params)
+        
+        pred_matrix = torch.stack(predictions, dim=1)
+        entropy     = calculate_entropy(pred_matrix)
+        accuracy    = (pred_matrix == target.view(-1, 1)).float().mean(dim=1)
+        
+        for e, a in zip(entropy, accuracy):
+            result.append((e.item(), a.item()))
+
+        if (batch + 1) % 50 == 0:
+            print(f'Batch {batch + 1}/{len(dataset)} completed')
+
+    print(f'Batch {batch+1}/{len(dataset)} completed')
+    print(f'Testing model with noise scale {sigma:.2f} completed\n')
+    return result
+
+def plot_entropy_prob(ent_prob, sigma, acc, iterations, SAVE_PLOT=False):
+    """
+    Plots the list of probability/entropy parts on the x/y-axis respectively.
+    """
+    entropy, probability = zip(*ent_prob)
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(probability, entropy, alpha=0.1)
+    plt.xlabel('Probability')
+    plt.ylabel('Entropy')
+    plt.title(f'Entropy to Probability (sigma={sigma}, iterations={iterations}, {acc}%)')
+    plt.grid()
+    plt.ylim(-0.1, np.log(10) + 0.1)
+    plt.xlim(-0.1, 1.1)
+    plot_curve(classes=10)
+    if SAVE_PLOT: plt.savefig(f'plots/entropy_prob_sigma_{sigma:.2f}.pdf')
+    else: plt.show()
+
+
+def plot_curve(classes):
+    """
+    This call plots the domain of the amount of given classes.
+    (Beware there exists off-by-one, this is handled in the function)
+    """
+    classes -= 1
+    # Arc-function at bottom
+    x = np.linspace(0.0001, 0.9999, 10000)
+    y = -x * np.log(x) - (1 - x) * np.log(1 - x)
+    plt.plot(x, y, color='orange')
+    x = np.linspace(0.0001, 0.9999, 10000)
+    y = -x * np.log(x) - (1 - x) * np.log(1 - x)
+
+    # Higher top function
+    y2 = []
+    for p in x:
+        a = -p * np.log(p)
+        for _ in range(classes):
+            a -= (1 - p) / (classes) * np.log((1 - p) / classes)
+        y2.append(a)
+
+    plt.plot(x, y, color="orange")
+    plt.plot(x, y2, color="orange")
+
+    # This plots the vertical line
+    plt.plot([0, 0], [0, y2[0]], color="orange")
+
+def calculate_weighted_averages(data, window_size=0.25):
+    """
+    Calculates and prints the weighted average of probabilities for specified windows of entropy.
+    
+    :param data: A list of tuples, where each tuple is (entropy, probability)
+    :param window_size: The size of the entropy window to calculate averages for
+    """
+    # Determine the maximum entropy value from the data
+    max_entropy = max(data, key=lambda x: x[0])[0]
+    
+    start = 0
+    result = []
+    
+    while start <= max_entropy:
+        end = start + window_size
+        
+        # Filter data for current window
+        filtered_data = [t for t in data if start <= t[0] < end]
+        
+        # Continue to next window if no data is found within the current window
+        if not filtered_data:
+            start += window_size
+            continue
+        
+        # Add count 1 for each data point in the window (think frequency of the probability)
+        weights = [1 for _ in filtered_data]  
+        
+        # Calculate the weighted average for the filtered data
+        weighted_average_filtered = sum(p * w for (_, p), w in zip(filtered_data, weights)) / sum(weights)
+        
+        # Print the weighted average for the current window
+        print(f"Window {start:.2f} to {end:.2f}: Weighted Average = {weighted_average_filtered}")
+        result.append(((start+end)/2, weighted_average_filtered)) 
+        start += window_size
+    return result
+
+def plot_weighted_averages(data):
+    """
+    Plots the weighted averages of probabilities for specified windows of entropy.
+    
+    :param data: A list of tuples, where each tuple is (entropy, probability)
+    """
+    plt.figure(figsize=(10, 6))
+    plt.ylabel('Entropy')
+    plt.xlabel('Weighted Average Probability')
+    plt.title('Weighted Averages of Probabilities for Specified Windows of Entropy')
+    plt.grid()
+
+    for lst, sigma in data:
+        sorted_data = sorted(lst, key=lambda x: x[0])
+        entropy, probability = zip(*sorted_data)
+        plt.plot(probability, entropy, marker='o', label=f'Sigma:{sigma:.2f}')
+
+    plt.legend()
+    plt.show()
+
+
+
+
+
+
+
+
