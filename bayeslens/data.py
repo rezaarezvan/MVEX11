44b78ec5333e52566be9dfbdad9b7ecfa2f5be68
rezaarezvan <rezvanr2378@gmail.com>
2024-03-11 16:27:39 +0100

    bayeslens: remove gtsrb

--- bayeslens/data.py
+++ bayeslens/data.py
@@ -1,9 +1,9 @@
 import os
 import json
+import torch
 from PIL import Image
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
-import torch
 
 
 class SSLADDataset(Dataset):
