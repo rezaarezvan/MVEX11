ec515de9eb73c2834e8d338c2590c7c8e73d29e5
rezaarezvan <rezvanr2378@gmail.com>
2024-03-13 08:19:05 +0100

    bayeslens: add utils/ (working); remove conformal pred

--- bayeslens/model_vit.py
+++ bayeslens/model_vit.py
@@ -1,10 +1,13 @@
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.distributions import Normal
 from data import load_data
 from torchvision.models import vit_b_16, ViT_B_16_Weights
 from torchvision import transforms
+from utils.entrop import test_model_noise, calculate_weighted_averages, plot_weighted_averages, plot_entropy_prob, compute_k
+from utils.model import load_model, save_model
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -103,7 +106,28 @@ def main():
         model.parameters(), lr=0.001, weight_decay=1e-5)
     criterion = nn.CrossEntropyLoss()
 
-    train_model(model, train, val, optimizer, criterion)
+    train_model(model, train, val, optimizer, criterion, num_epochs=1)
+
+    iterations = 2
+    entropies = []
+    weighted_average = []
+
+    sigmas = np.arange(0.1, 1, 0.1)
+    sigmas = np.append(sigmas, [1, 10, 100, 500])
+
+    for sigma in sigmas:
+        print(f"Sigma: {sigma}")
+        entropy = test_model_noise(model, test, sigma=sigma, iters=iterations)
+        weighted_average.append((calculate_weighted_averages(entropy), sigma))
+        entropies.append(entropy)
+        for lam in [0.1, 0.5, 1]:
+            print(f"Lambda: {lam}, K: {compute_k(entropy, _lambda=lam)}")
+        print('-----------------------------------\n')
+
+    plot_weighted_averages(weighted_average)
+    for entropy, sigma in zip(entropies, sigmas):
+        plot_entropy_prob(entropy, sigma, 10,
+                          iterations)
 
 
 if __name__ == "__main__":
