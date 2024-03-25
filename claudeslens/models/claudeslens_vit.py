c51ed763883b0a3f37e245c66ed9cc720517b7ac
rezaarezvan <rezvanr2378@gmail.com>
2024-03-25 21:18:54 +0100

    claudeslens: fix ViT models; (jag trollar ju)

--- claudeslens/models/claudeslens_vit.py
+++ claudeslens/models/claudeslens_vit.py
@@ -6,12 +6,19 @@ class ClaudesLens_ViT(nn.Module):
     def __init__(self, num_classes=6, num_inputs=None, num_channels=None):
         super(ClaudesLens_ViT, self).__init__()
         self.vit = vit_b_16(weights=ViT_B_16_Weights.DEFAULT)
-        self.classifier = nn.Linear(
-            self.vit.heads.head.in_features, num_classes)
+
+        for params in self.vit.parameters():
+            params.requires_grad = False
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.vit.heads.head.in_features, 512),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(512, num_classes)
+        )
+
         self.vit.heads.head = self.classifier
-        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.vit(x)
-        x = self.softmax(x)
         return x
--- claudeslens/models/vit_b_16.py
+++ claudeslens/models/vit_b_16.py
@@ -6,11 +6,13 @@ class Pretrained_ViT(nn.Module):
     def __init__(self, num_classes=6, num_inputs=None, num_channels=None):
         super(Pretrained_ViT, self).__init__()
         self.vit = vit_b_16(weights=ViT_B_16_Weights.DEFAULT)
+
+        for params in self.vit.parameters():
+            params.requires_grad = False
+
         self.vit.heads.head = nn.Linear(
             self.vit.heads.head.in_features, num_classes)
-        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = self.vit(x)
-        x = self.softmax(x)
         return x
