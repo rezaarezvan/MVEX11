2de63bf045966bbc53f58de66a69aefda89c76c9
rezaarezvan <reza@rezvan.xyz>
2024-03-29 20:42:59 +0100

    fix setup and reqs

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 directory = Path(__file__).resolve().parent
 with open(directory / 'README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-setup(name='claudeslens',
-      version='0.0.1',
-      license='MIT',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      packages=['claudeslens'],
-      classifiers=[
-          "Programming Language :: Python :: 3",
-      ],
-      install_requires=["torch"],
-      python_requires='>=3.8',
-      extras_require={
-          'testing': [
-              "pytest",
-              "tabulate",
-          ]
-      },
-      include_package_data=True)
+with open(directory / 'requirements.txt', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='claudeslens',
+    version='0.0.1',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=requirements,
+    python_requires='>=3.8',
+    extras_require={
+        'testing': [
+            "pytest",
+            "tabulate",
+        ]
+    },
+    include_package_data=True
+)
