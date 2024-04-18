8dbc05b05f99d0b69eccc6e042496765d2b7bfe4
rezaarezvan <rezvanr2378@gmail.com>
2024-04-18 21:10:15 +0200

    claudeslens: `__init__.py` for models

---
+++ claudeslens/models/__init__.py
@@ -0,0 +1,13 @@
+from claudeslens.models.pretrained_vit import Pretrained_ViT_B_16
+from claudeslens.models.claudeslens_vit import ClaudesLens_ViT
+from claudeslens.models.pretrained_convnext import Pretrained_ConvNext
+from claudeslens.models.claudeslens_convnext import ClaudesLens_ConvNext
+from claudeslens.models.claudeslens_logistic import ClaudesLens_Logistic
+
+__all__ = [
+    'Pretrained_ViT_B_16',
+    'ClaudesLens_ViT',
+    'Pretrained_ConvNext',
+    'ClaudesLens_ConvNext',
+    'ClaudesLens_Logistic'
+]
